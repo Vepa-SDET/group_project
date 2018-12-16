76f43af47dadc3d65ae85d802ba501e751a00bfb
Kadriye <kadriyetice@gmail.com>
2018-12-15 23:56:01 -0600

    HomePage update

--- Pages/Home.page.js
+++ Pages/Home.page.js
@@ -7,6 +7,11 @@ let HomePage=function(){
     this.homePageGitHubLink=$("[class='fa fa-github']");
     this.homePageQuestionLink=$("[class='fa fa-question-circle']");
 
+    // Kadriye
+    this.HomePageLogo=$(".title");
+    this.email=element(by.name("email"));
+    this.password=element(by.name("password"));
+    this.signinButton=$(".button.is-dark");
     
 
 }
