0dc0b62219c3f619bb796b41ef64f18a316a4392
Vepa-SDET <vepaorash@gmail.com>
2018-12-15 23:21:34 -0600

    framework upload

---
+++ Pages/Home.page.js
@@ -0,0 +1,13 @@
+require('../Utilities/CustomLocators.js');
+let HomePage=function(){
+    this.homePageLogo=$(".title");
+    this.homePageEmailLoginBar=$("[placeholder='email']");
+    this.homePagePasswordBar=$("[placeholder='password']");
+    this.homePageSignInButton=$("button[type='submit']");
+    this.homePageGitHubLink=$("[class='fa fa-github']");
+    this.homePageQuestionLink=$("[class='fa fa-question-circle']");
+
+    
+
+}
+module.exports=new HomePage();
---
+++ Tests/HomePage.spec.js
@@ -0,0 +1,70 @@
+require('../Utilities/CustomLocators.js');
+let HomePage= require('../Pages/Home.page.js');
+let Base = require('../Utilities/Base.js');
+let Data = require('../TestData/data.json');
+
+describe('BookIT Home Page test scripts', () => {
+    let browserWindows="";
+    beforeAll(() => {
+        Base.navigateToHome();
+    });
+
+    it('should print out the Title', () => {
+        expect(browser.getTitle()).toEqual("cybertek-bnb");
+        browser.getTitle().then((t)=>{
+            console.log(t);
+        });
+        
+    });
+
+    it('should check Sign in text on the Home Page', () => {
+    expect(HomePage.homePageLogo.getText()).toEqual(Data.HomePageLogo.Text);        
+    expect(HomePage.homePageLogo.getCssValue("color")).toEqual(Data.HomePageLogo.Color);
+    
+    });
+
+    it('should verify Login functionality with invalid credentials', () => {
+        HomePage.homePageEmailLoginBar.sendKeys(Data.invalidUser.email);
+        HomePage.homePagePasswordBar.sendKeys(Data.invalidUser.password);
+        HomePage.homePageSignInButton.click();
+        expect(HomePage.homePageLogo.getText()).toEqual(Data.HomePageLogo.Text);
+
+    });
+
+    fit('should verify "Git Hub" icon is forwarding to Git Hub website when clicked', () => {
+        
+        HomePage.homePageGitHubLink.click();
+        let gitWindow="";
+        browser.driver.getAllWindowHandles().then((h)=>{
+            browserWindows=h;
+            gitWindow=h[1];
+        });
+        
+        browser.driver.switchTo().window(gitWindow).then(()=>{
+            browser.waitForAngularEnabled(false);
+            expect(browser.getTitle()).toEqual(Data.gitHubTitle.text);
+        });
+        
+    });
+
+    fit('should verify "Question" icon swithced to Mailto window when clicked', () => {
+        let questionWindow="";
+        browser.close().then(()=>{
+            browser.switchTo().window( browserWindows[0]);
+        });    
+        HomePage.homePageQuestionLink.click();
+        browser.driver.getAllWindowHandles().then((h)=>{
+            browserWindows=h;
+            questionWindow=h[1];
+        });
+        
+        browser.driver.switchTo().window(questionWindow).then(()=>{
+            browser.waitForAngularEnabled(false);
+            browser.getCurrentUrl().then((u)=>{
+                console.log(u);
+            });
+            // expect(browser.getTitle()).toEqual(Data.gitHubTitle.text);
+        });
+        
+    });
+});
