9174230315c29639b87d1e7d6d7866aa336e1d77
feride <ozdemirfd@gmail.com>
2018-12-17 10:42:21 -0600

    added 3 test cases

--- Pages/Rooms.page.js
+++ Pages/Rooms.page.js
@@ -1,6 +1,7 @@
 
 var RoomsPage = function(){
     this.roomsPageScheduleTable=$("[class='table is-fullwidth is-transparent borderless']");
+    this.roomsPageScheduleHeader=$(".container>h1");
     
     
 }
