2f4d5dfc8887f35583b0dea9732bacf30efaefff
feride <ozdemirfd@gmail.com>
2018-12-17 21:55:46 -0600

    update

--- Pages/Map.page.js
+++ Pages/Map.page.js
@@ -4,13 +4,36 @@ var MapPage = function(){
     this.mapPageMapNameElement = $('.subtitle');
     this.mapPageMeruLink=$("[id='room-124']");
     this.mapPageCyberTekBNBLogo=$(".intro-img");
+
+    this.mapPagebyBugbusters7Logo=$(".content.has-text-centered");
+    this.mapPageScheduleDropDown=element(by.xpath("//a[text()='schedule']"));
+    this.mapPageScheduleGeneralOption=element(by.xpath("//a[text()='general']"));
+    this.mapPageMyDropDown=$$("[class='navbar-link']").get(1);
+    this.mapPageMySelfOption=element(by.xpath("//a[text()='self']"));
+    this.mapPageMyTeamOption= element(by.xpath("//a[text()='team']"));
+    this.mapPageMyTeamOption=element(by.xpath("//a[text()='team']"));
+
     this.mapPagebyBugbusters7Logo=$(".content.has-text-centered"); 
     this.mapLoby=$("#lobby-va-dark-side");
     this.mapStudyArea=$("#study_area-121");
     this.map4stayArea=$("#four_stay-121");
     this.mapTopMenu=$(".navbar-menu.is-transparent");
     this.mapMapText=$(".navbar-end>a:nth-child(1)");
-    this.mapScheduleText=$(".navbar-end>div>a:nth-child(1)")
+    this.mapScheduleLink=element(by.linkText('schedule'));
+
+    this.halfDome=$("a#room-122");
+    this.drenali=$("a#room-123");
+    this.meru=$("a#room-123");
+
+    this.huntLink=element(by.linkText('hunt'));
+    this.huntLinkMy=element(by.linkText('my'));
+    this.huntLinkGeneral=element(by.linkText('general'));
+    this.myLink=element(by.linkText('my'));
+    this.myLinkSelf=element(by.linkText('self'));
+    this.myLinkTeam=element(by.linkText('team'));
+    this.myLinkSignout=element(by.linkText('sign out'));
+  
+
 }
 
 module.exports = new MapPage();
